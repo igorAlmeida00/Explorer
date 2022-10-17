2fc3103bb0b908b17bf2c1dd956e33bc1cf10ec8
igorAlmeida00 <igor77alme@outlook.com>
2022-10-16 22:38:33 -0300

    update AppError

--- Stage-08/API/src/controllers/UsersController.js
+++ Stage-08/API/src/controllers/UsersController.js
@@ -4,6 +4,10 @@ class UsersController {
   create(request, response) {
     const { name, email, password} = request.body;
 
+    if(!name) {
+      throw new AppError('Falta Alguma(s) Informações');
+    }
+
     response.status(201).json({ name, email, password});
   }
  
